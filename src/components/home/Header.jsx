69f092e2cb63a5310494f023eb9b1e9fde5fc0dd
RadojkoRadulovic <radulovicradojko@gmail.com>
2024-04-25 15:10:06 -0400

    111

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -1,8 +1,8 @@
 import BgVideo from "../../assets/videos/pearTreeBlossom.mp4";
 function Header() {
   return (
-    <header className="w-100 mainHeader">
-        <video className="w-100"  src={BgVideo} autoPlay loop muted/>
+    <header className="w-100 mainHeader -z-50">
+        <video className="w-100 -z-50"  src={BgVideo} autoPlay loop muted/>
     </header>
   )
 }
